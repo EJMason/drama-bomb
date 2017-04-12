baae9d105388ec662da83daf3ef45f1b58d740fa
EJMason <eliotjunior@gmail.com>
2017-04-11 21:55:07 -0700

    [feat] modularized server.js file

--- server/server.js
+++ server/server.js
@@ -1,30 +1,22 @@
 require('dotenv').config()
 const express = require('express')
 const path = require('path')
-const bodyParser = require('body-parser')
 const chalk = require('chalk')
-const cors = require('cors')
-const morgan = require('morgan')
+require('./database')
 
+const port = process.env.PORT || 2020
 const app = express()
-const http = require('http').Server(app)
 
 app.use(express.static(path.resolve(__dirname, '..', 'build')))
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(cors())
-app.use(morgan('short'))
-
+require('./middleware')(app)
 require('./routes')(app)
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'))
 })
 
-const port = process.env.PORT || 2020
-
-http.listen(port, () => {
+app.listen(port, () => {
   console.log(chalk.bgGreen.black(`listening on port ${port}`))
 })
 
-module.exports = http
+module.exports = app
