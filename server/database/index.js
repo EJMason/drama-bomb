fa6dad4d4d67feb671cdbdac6d759176c6e35a48
EJMason <eliotjunior@gmail.com>
2017-04-11 21:54:25 -0700

    [feat] Users Schema added

---
+++ server/database/index.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const User = require('./models/Users')
+mongoose.Promise = require('bluebird')
+
+
+mongoose.connect(process.env.DB_MONGO_CONN)
+
+mongoose.connection.on('open', () => {
+  // User.create({ user_id: 111, screen_name: 'apples' }).then(() => {
+  //   User.find({ user_id: 111 }).exec((a, b) => {
+  //     console.log(a)
+  //     console.log(b)
+  //   })
+  // })
+})
---
+++ server/database/models/Users.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const haterSchema = new Schema({
+  user_id: Number,
+  screen_name: String,
+  image: String,
+  first_name: String,
+  family_name: String,
+  passive: Boolean,
+  aggressive: Boolean,
+  apology: Boolean,
+  demon_mode: { type: Boolean, default: false },
+}, { autoIndex: false })
+
+const userSchema = new Schema({
+  user_id: Number,
+  screen_name: String,
+  token: String,
+  token_secret: String,
+  friends_ids: [Number],
+  haters: [haterSchema],
+}, { autoIndex: false })
+
+const Users = mongoose.model('User', userSchema)
+
+Users.on()
+
+module.exports = Users
