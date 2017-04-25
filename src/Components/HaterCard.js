ef99c06b55709a30363a04a74d6d6144224080e8
EJMason <eliotjunior@gmail.com>
2017-04-25 16:30:29 -0700

    Animations are now begining to come together * Added react-animate library for performant animations * Tested functionality on card components

--- src/Components/HaterCard.js
+++ src/Components/HaterCard.js
@@ -1,22 +1,63 @@
 import React, { Component } from 'react'
+import { Motion, spring } from 'react-motion'
 
 import './Styles/css/HaterCard.css'
 
 class HaterCard extends Component {
   constructor(props) {
     super(props)
+    this.state = { open: false }
+  }
 
-    this.state = {
-      temp: 'a',
-    }
+  handleMouseDown = () => {
+    this.setState({ open: !this.state.open })
+  }
+
+  handleTouchStart = e => {
+    e.preventDefault()
+    this.handleMouseDown()
   }
 
   render() {
     return (
-      <div className="card-container">
-        <div className="color-container">
-          .
-        </div>
+      <div>
+        <button
+          onMouseDown={this.handleMouseDown}
+          onTouchStart={this.handleTouchStart}
+        >
+          Toggle
+        </button>
+        <Motion
+          style={{
+            x: spring(this.state.open ? 35 : 0),
+            y: spring(this.state.open ? 0 : 3),
+            z: spring(this.state.open ? 105 : 70),
+          }}
+        >
+          {({ x, y, z }) =>
+            <div
+              className="card-container"
+              style={{
+                height: `${z}px`,
+              }}
+            >
+              {x}
+              <div
+                className="button-container"
+              >
+              yar
+              </div>
+              <div
+                className="color-container"
+                style={{
+                  borderRadius: `3px 3px ${y}px ${y}px`,
+                }}
+              >
+                .
+              </div>
+            </div>
+          }
+        </Motion>
       </div>
     )
   }
