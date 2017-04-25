3c82f64263efbf686e7181c3b159f451fc005ea0
EJMason <eliotjunior@gmail.com>
2017-04-25 15:06:57 -0700

    Added Card compoment for sidebar

---
+++ src/Components/HaterCard.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+import './Styles/css/HaterCard.css'
+
+class HaterCard extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      temp: 'a',
+    }
+  }
+
+  render() {
+    return (
+      <div className="card-container">
+        <div className="color-container">
+          .
+        </div>
+      </div>
+    )
+  }
+}
+
+export default HaterCard
