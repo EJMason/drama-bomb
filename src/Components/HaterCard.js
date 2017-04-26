247101109ebf5c301560e54b7edda65264f9e4f3
EJMason <eliotjunior@gmail.com>
2017-04-25 17:43:53 -0700

    [feat] styling logic for animating component

--- src/Components/HaterCard.js
+++ src/Components/HaterCard.js
@@ -29,9 +29,9 @@ class HaterCard extends Component {
         </button>
         <Motion
           style={{
-            x: spring(this.state.open ? 35 : 0),
+            x: spring(this.state.open ? 80 : 50, { stiffness: 130, damping: 13 }),
             y: spring(this.state.open ? 0 : 3),
-            z: spring(this.state.open ? 105 : 70),
+            z: spring(this.state.open ? 105 : 70, { stiffness: 130, damping: 13 }),
           }}
         >
           {({ x, y, z }) =>
@@ -45,7 +45,26 @@ class HaterCard extends Component {
               <div
                 className="button-container"
               >
-              yar
+                <div
+                  className="button-row"
+                >
+                  <img
+                    className="emoji" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1212929/demon.svg"
+                    alt="hater-button"
+                  />
+                  <img
+                    className="emoji" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1212929/apology.png"
+                    alt="hater-button"
+                  />
+                  <img
+                    className="emoji" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1212929/aggressive.svg"
+                    alt="hater-button"
+                  />
+                  <img
+                    className="emoji" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1212929/passive.svg"
+                    alt="hater-button"
+                  />
+                </div>
               </div>
               <div
                 className="color-container"
@@ -53,7 +72,14 @@ class HaterCard extends Component {
                   borderRadius: `3px 3px ${y}px ${y}px`,
                 }}
               >
-                .
+                <img
+                  className="profile-image"
+                  src="http://staging1.dima.co.nz/wp-content/uploads/2016/07/4.jpg"
+                  alt="hater"
+                  style={{
+                    height: `${x}px`,
+                  }}
+                />
               </div>
             </div>
           }
