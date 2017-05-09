62a06f1e118a4bae2ef92b62c467f296b8897a69
EJMason <eliotjunior@gmail.com>
2017-05-08 19:21:17 -0700

    [feat] cron task now functioning, starts and stops when users not logged in

--- server/utilities/cronUtil.js
+++ server/utilities/cronUtil.js
@@ -13,25 +13,32 @@ const throwErr = (statusCode, message, method, defaultError = null) => {
   }
 }
 
+module.exports.checkUsersLoggedIn = () => {
+  return new Promise((resolve, reject) => {
+    redis.getAllActiveUserIds()
+      .then(users => {
+        const groups = cronService.groupsUsers(users)
+        resolve(groups.length ? true : false)
+      })
+      .catch(reject)
+  })
+}
+
 module.exports.cronCheck = async () => {
   try {
     // get all of the users from redis, that means they are logged in, groups of 100
     const groupsOfUsers = cronService.groupsUsers(await redis.getAllActiveUserIds())
-
-   /*
-      What should I do here if groupsOfUsers is empty?
-    */
 
     // generate the twitter queries for each group, request twitter api
     const arrOfResponses = await Promise.all(cronService.genTwitterQueries(groupsOfUsers))
 
     // compare twitter to redis to see if anyone was unfollowed
     const changedUsers = cronService.compareItems(groupsOfUsers, arrOfResponses)
-
     if (changedUsers.length) {
       const result = await Promise.all(changedUsers.map(user => friendsUtil.updateFromTwitter(user)))
-      console.log('THIS IS THE RESULT: ', result)
+      return result
     }
+    return []
   } catch (err) {
     console.error('ERROR IN CRONUTIL: ', err)
     throw throwErr(null, 'Error in chron job', 'cronCheck', err)
