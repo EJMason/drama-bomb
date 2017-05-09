0149f0dbbd28355ff75a3635b4a4575b4f8d74d4
EJMason <eliotjunior@gmail.com>
2017-05-08 22:46:00 -0700

    [fix] modularized all logic associated with cron task

---
+++ server/cron/cronTask.js
@@ -0,0 +1,74 @@
+const CronJob = require('cron').CronJob
+const chalk = require('chalk')
+const EventEmitter = require('events')
+
+const util = require('../utilities/cronUtil')
+
+class CronTask {
+  constructor() {
+    this.emitter = new EventEmitter()
+    this.updateId = 10000
+
+    this.ticker = this.ticker.bind(this)
+    this.initializer = this.initializer.bind(this)
+
+    this.workerBee = new CronJob({
+      cronTime: '*/10 * * * * *',
+      onTick: this.ticker,
+      onComplete: () => {},
+      start: false,
+    })
+  }
+
+  initializer() {
+    console.log(chalk.bgBlue.magenta('Cron Task Begin'))
+    this.workerBee.start()
+  }
+
+  resumeTask() {
+    if (!this.workerBee.running) {
+      console.log(chalk.bgBlue.magenta('Resuming Cron Task...'))
+      this.workerBee.start()
+    } else {
+      console.log(chalk.bgBlue.magenta('Cron Task Already running'))
+    }
+  }
+
+  stopTask() {
+    console.log(chalk.bgBlue.magenta('Pausing Cron Task..., nobody logged in'))
+    this.workerBee.stop()
+  }
+
+  genId() {
+    this.updateId++
+    return this.updateId
+  }
+
+  ticker() {
+    console.log(chalk.bgBlue.magenta('Cron Task: Checking logged in Users...'))
+    util.cronCheck().then(data => {
+      if (data) {
+        this.emitter.emit('updated_users', data)
+      }
+    }).catch(err => {
+      console.error('THERE WAS AN ERROR: ', err)
+    })
+  }
+}
+
+// ------------ Triggers -------------- //
+
+// const cron = new CronTask()
+
+// sub.on('pmessage', (pattern, channel) => {
+//   console.log('THIS IS THE EVENT: ', channel)
+//   if (channel === '__keyevent@0__:expired') {
+//     redis.decr('usercount').then(numOfUsers => {
+//       if (!numOfUsers) {
+//         cron.stopTask()
+//       }
+//     })
+//   }
+// })
+
+module.exports = CronTask
---
+++ server/cron/index.js
@@ -0,0 +1,34 @@
+const CronTask = require('./cronTask')
+const Redis = require('ioredis')
+
+const redis = require('../database/redis')
+
+const sub = new Redis()
+sub.psubscribe('__keyevent@0__:expired')
+sub.psubscribe('__keyevent@0__:set')
+
+const cron = new CronTask()
+
+cron.initializer()
+
+sub.on('pmessage', (pattern, channel) => {
+  if (channel === '__keyevent@0__:expired') {
+    redis.decr('usercount')
+      .then(numOfUsers => {
+        if (!numOfUsers) {
+          cron.stopTask()
+        }
+      })
+      .catch(err => { throw err })
+  } else if (channel === '__keyevent@0__:set') {
+    redis.get('usercount')
+      .then(count => {
+        if (count) {
+          cron.resumeTask()
+        }
+      })
+      .catch(err => { throw err })
+  }
+})
+
+module.exports = cron
